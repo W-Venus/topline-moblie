002145a4a8d953a550038870fecb7670298ff942
W-Venus <wc15236485854@163.com>
2019-07-08 22:07:30 +0800

    引入并配置vant

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  // 配置vant
+  plugins: [
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
   ]
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 测试引入是否成功
+import { Button } from 'vant'
 
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
