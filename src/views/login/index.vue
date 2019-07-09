92946be674af828077de5ca66cef5f3f4839a998
W-Venus <wc15236485854@163.com>
2019-07-09 16:18:14 +0800

    login布局样式完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    <p>login 页面</p>
+    <!-- 头部 -->
+    <van-nav-bar class="login-nav" title="登录"/>
+    <!-- 表单 -->
+    <form>
+        <van-cell-group>
+            <van-field
+                v-model="user.moblie"
+                required
+                clearable
+                label="手机号"
+                placeholder="请输入手机号"
+            />
+
+            <van-field
+                v-model="user.code"
+                type="password"
+                label="验证码"
+                placeholder="请输入验证码"
+                required
+            />
+        </van-cell-group>
+        <div class="login-btn-box">
+            <van-button class="login-btn" type="info" size="large">登录</van-button>
+        </div>
+    </form>
   </div>
 </template>
 
@@ -10,6 +34,10 @@ export default {
 
   data () {
     return {
+      user: {
+        moblie: '',
+        code: ''
+      }
     }
   },
 
@@ -18,5 +46,17 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+.van-nav-bar__title {
+    color: #fff;
+}
+.login-nav {
+    background-color: #1989fa;
+    margin-bottom: 10px;
+}
+.login-btn-box {
+    padding: 25px;
+    .login-btn {
+        border-radius: 10px;
+    }
+}
 </style>
