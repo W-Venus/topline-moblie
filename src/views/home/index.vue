ac33177fd547ebc22035d84400925edc03dd787e
W-Venus <wc15236485854@163.com>
2019-07-09 22:01:26 +0800

    首页样式完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <!-- 头部 -->
-    <van-nav-bar class="home-nav" title="首页"/>
+    <van-nav-bar class="home-nav" title="首页" fixed/>
     <!-- /头部 -->
-    <van-tabs v-model="active">
-      <van-tab title="标签 1">
+    <van-tabs class="channel-tabs" v-model="active">
+      <van-tab title="推荐">
         <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
           <!-- 列表 -->
           <van-list
@@ -22,17 +22,16 @@
         <!-- /列表 -->
         </van-pull-refresh>
       </van-tab>
-      <van-tab title="标签 2">内容 2</van-tab>
-      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="热门话题">内容 2</van-tab>
+      <van-tab title="科技动态">内容 3</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
     </van-tabs>
-    
     <!-- 底部 -->
     <van-tabbar v-model="footerTabs">
-      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="search">标签</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">问答</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
     </van-tabbar>
     <!-- /底部 -->
   </div>
@@ -47,8 +46,8 @@ export default {
       active: 0,
       footerTabs: 0,
       list: [],
-      loading: false, // 列表上拉加载
-      finished: false, // 列表加载结束
+      loading: false, // 列表上拉加载更多
+      finished: false, // 控制列表加载是否结束
       isLoading: false // 下拉刷新加载完成
     }
   },
@@ -79,5 +78,21 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+// /deep/ 的作用（深度作用选择器）
+.channel-tabs /deep/ .van-tabs__wrap {
+  position: fixed;
+  top: 92px;
+}
+.channel-tabs /deep/ .van-tabs__content {
+  margin-top: 92px;
+}
+.van-nav-bar__title {
+  color: #fff;
+}
+.home-nav {
+  background-color: #3296fa;
+}
+.van-list {
+  margin-bottom: 100px;
+}
 </style>
