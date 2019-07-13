8398731cd5552946945c7e58e9000069708fa1fe
W-Venus <wc15236485854@163.com>
2019-07-13 15:04:27 +0800

    创建弹出层组件

---
+++ src/views/home/components/channel.vue
@@ -0,0 +1,25 @@
+<template>
+  <van-popup
+    v-model="show"
+    position="bottom"
+    :style="{ height: '90%' }"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Channel',
+  props: {
+      
+  },
+  data () {
+    return {
+      show: false
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped></style>
