47b916fbf72a3f7c74264606667707d231faa020
W-Venus <wc15236485854@163.com>
2019-07-13 17:17:41 +0800

    封装所有频道接口API

--- src/api/channel.js
+++ src/api/channel.js
@@ -2,10 +2,17 @@
 
 // 加载axios请求模块
 import request from '@/utils/request'
-
+// 请求默认频道数据
 export const getUserChannel = () => {
   return request({
     method: 'GET',
     url: '/app/v1_0/user/channels'
+  })
+}
+// 请求所有频道数据
+export const getAllChannel = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/channels'
   })
 }
