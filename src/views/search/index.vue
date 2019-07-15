efae48b016fe30c95f9ff6b2b63b0ecd942a9b92
W-Venus <wc15236485854@163.com>
2019-07-15 21:40:46 +0800

    创建搜索页面&配置路由

---
+++ src/views/search/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <van-search
+      v-model="value"
+      placeholder="请输入搜索关键词"
+      show-action
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+
+  data () {
+    return {
+      value: ''
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+</style>
