d01a960a1291d9ae2ebaca0c5969f4f6873b7a9e
W-Venus <wc15236485854@163.com>
2019-07-16 13:44:21 +0800

    搜索页面-设置关键字高亮

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -6,8 +6,17 @@
       show-action
     />
     <van-cell-group>
-      <van-cell v-for="item in searchList" :key="item" :title="item" />
+      <van-cell v-for="item in searchList" :key="item" :title="item">
+        <!-- 使用插槽,定制内容 -->
+        <div slot="title" v-html="highLight(item, searcheText)"></div>
+      </van-cell>
     </van-cell-group>
+    <!-- <van-cell-group title="历史记录">
+      <van-cell title="1" />
+      <van-cell title="2" />
+      <van-cell title="3" />
+      <van-cell title="4" />
+    </van-cell-group> -->
   </div>
 </template>
 
@@ -20,13 +29,14 @@ export default {
 
   data () {
     return {
+      isShowHistory: false, // 控制显示历史记录
       searcheText: '', // 搜索框绑定的内容
       searchList: [] // 联想建议自动补全数据列表
     }
   },
   watch: {
     // 防抖
-    searcheText: debounce( async function (val) {
+    searcheText: debounce(async function (val) {
       // 去除首尾空格
       val = val.trim()
       // 判断
@@ -37,9 +47,14 @@ export default {
       // 如果不为空,则发请求,请求联想建议
       const data = await getSuggestions(val)
       this.searchList = data.options
-    },500)
+    }, 500)
   },
-  methods: {}
+  methods: {
+    // 设置关键字高亮
+    highLight (text, keyword) {
+      return text.toLowerCase().split(keyword).join(`<span style="color: red;">${keyword}</span>`)
+    }
+  }
 }
 </script>
 
